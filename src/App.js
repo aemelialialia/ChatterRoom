ee7ee94b730809298c386829144b452d41274524
Aiwen Zhang <emily.aiwen@gmail.com>
2019-07-05 22:01:22 -0700

    Got NamePicker to work but no style

--- src/App.js
+++ src/App.js
@@ -7,13 +7,26 @@ import NamePicker from './NamePicker.js'
 class App extends React.Component {
 
   state={
-    messages: []
+    messages: [],
+    name:"",
+    editName: true
   }
 
   sendMessage = (m) => {
     var messages = [...this.state.messages, m]
     this.setState({messages})
   }
+
+  setEditName = (boolean) => {
+    var editName = boolean
+    this.setState({editName})
+  }
+
+  changeName = (e) => {
+    var name = e
+    this.setState({name})
+  }
+
   render() {
     var {messages} = this.state
     console.log(messages);
@@ -23,17 +36,25 @@ class App extends React.Component {
         <header className="header">
           <img src={logo} className="logo" alt="" />
         Chatter
-        <NamePicker />
+        <div className="name-input">
+        <NamePicker 
+          name={this.state.name}
+          editName={this.state.editName}
+          changeName={this.changeName}
+          setEditName={this.setEditName} />
+          </div>
         </header>
         <TextInput className="text-input" alt=""
-          sendMessage={this.sendMessage }/>
+          sendMessage={this.sendMessage}/>
         <main className="chat">
             <div className="chat-message them">Are we meeting today?</div>
             <div className="chat-message me">yes, what time suits you?</div>
             <div className="chat-message them">I was thinking after lunch</div>
             <div className="chat-message me">Perfect, see you later!</div>
           {messages.map((m,i) => {
-            return <div key={i} className="chat-message me">{m}</div>
+            return <div key={i} className="chat-message me">
+              <span>{m}</span>
+            </div>
           })}
         </main>
       </div>
