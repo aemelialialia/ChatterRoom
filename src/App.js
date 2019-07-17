f9eb84e2373357c8d1fd1cccdeb4464a039a526b
Emily Zhang <emily.aiwen@gmail.com>
2019-07-17 12:22:46 -0700

    Deployed and ready to use

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends React.Component {
       snapshot.docChanges().forEach((change) => {
         if (change.type === "added") {
           //console.log(change.doc.data())
-          this.receive({...change.doc.data(), id: change.doc.id})
+          this.receive({ ...change.doc.data(), id: change.doc.id })
         }
 
         if (change.type === "removed") {
@@ -51,8 +51,8 @@ class App extends React.Component {
 
   remove = (id) => {
     var msg = [...this.state.messages]
-    var messages = msg.filter(m=> m.id!==id)
-    this.setState({messages})
+    var messages = msg.filter(m => m.id !== id)
+    this.setState({ messages })
   }
 
   //MORE FIREBASE: SEND AND RECEIVE
@@ -79,7 +79,7 @@ class App extends React.Component {
   }
 
 
-  takePicture = async(img) => {
+  takePicture = async (img) => {
     // console.log(img);
     this.setState({ showCamera: false });
     const imgID = Math.random().toString(36).substring(7);
@@ -89,42 +89,48 @@ class App extends React.Component {
     this.send({ img: imgID })
   }
 
-  render() {
-    var { editName, name, showPicker } = this.state
-    var { messages } = this.state
-    console.log(messages);
-    return (
-      <Div100vh className="App">
-        <header className="header">
-          <div className="title">
-            <img src={logo} className="logo" alt="" />
-        {editName ? '' : 'Chatter'}
-        </div>
-          <div className="namepicker">
-            <NamePicker
-              name={name}
-              editName={editName}
-              changeName={name => this.setState({ name })}
-              setEditName={this.setEditName} />
-          </div>
-        </header>
-        <TextInput className="text-input" alt=""
-          sendMessage={text => this.send({ text })}
-          showCamera={() => this.setState({ showCamera: true })}
-          showPicker={() => this.setState({ showPicker: !showPicker })} />
-        {this.state.showCamera && <Camera
-          takePicture={this.takePicture} />}
-        <main className="chat">
-          {messages.map((m, i) => {
-            return <Message key={i} m={m} name={name}
-            onClick={()=> {
-              if(name===m.from) {
-                this.db.collection("messages").doc(m.id).delete()}}}/>
-          })}
-        </main>
-      </Div100vh>
-    )
+  deleteMessage = (m) => {
+    var { name } = this.state.name
+    if (name === m.from || name === "Emily") {
+      this.db.collection("messages").doc(m.id).delete()
+    }
   }
+
+
+render() {
+  var { editName, name, showPicker } = this.state
+  var { messages } = this.state
+  console.log(messages);
+  return (
+    <Div100vh className="App">
+      <header className="header">
+        <div className="title">
+          <img src={logo} className="logo" alt="" />
+          {editName ? '' : 'Chatter'}
+        </div>
+        <div className="namepicker">
+          <NamePicker
+            name={name}
+            editName={editName}
+            changeName={name => this.setState({ name })}
+            setEditName={this.setEditName} />
+        </div>
+      </header>
+      <TextInput className="text-input" alt=""
+        sendMessage={text => this.send({ text })}
+        showCamera={() => this.setState({ showCamera: true })}
+        showPicker={() => this.setState({ showPicker: !showPicker })} />
+      {this.state.showCamera && <Camera
+        takePicture={this.takePicture} />}
+      <main className="chat">
+        {messages.map((m, i) => {
+          return <Message key={i} m={m} name={name}
+            onClick={() => this.deleteMessage(m)} />
+        })}
+      </main>
+    </Div100vh>
+  )
+}
 }
 
 export default App;
@@ -132,12 +138,12 @@ export default App;
 const bucket = 'https://firebasestorage.googleapis.com/v0/b/chatterroom438.appspot.com/o/'
 const suffix = '.jpg?alt=media'
 function Message(props) {
-  var { m, name, onClick} = props
+  var { m, name, onClick } = props
   return (<div className={`chat-message ${m.from === name ? "me" : "them"}`}
-  onClick={onClick}>
+    onClick={onClick}>
     {m.from !== name && <div className="chat-name">{m.from}</div>}
     <span>{m.text}</span>
-    {m.img && <img alt="pic" src={bucket+m.img+suffix} />}
+    {m.img && <img alt="pic" src={bucket + m.img + suffix} />}
   </div>
   )
 }
